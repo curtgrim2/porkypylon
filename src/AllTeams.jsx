4305c905c1c6a740b01d5e3544a06408a6578c45
curtgrim2 <curtisgrimsley18@gmail.com>
2026-02-25 10:43:07 -0500

    Fixed AllTeams.jsx large logo delay

--- src/AllTeams.jsx
+++ src/AllTeams.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import TeamsAPI from "./TeamsAPI";
 
 
@@ -8,14 +8,21 @@ import TeamsAPI from "./TeamsAPI";
 export default function AllTeams(){
     const [backInfo,setBackInfo]=useState();
 
-      function newBackground(){
+      const newBackground = (logo) =>{
+        setBackInfo(logo);
          console.log(backInfo);
          var fadedback=document.getElementById("fadedback");
          fadedback.style.backgroundImage=`url(${backInfo})`;
             fadedback.style.backgroundSize="cover";
             fadedback.style.backgroundPosition="center";
             fadedback.style.backgroundRepeat="no-repeat";
-      }
+      };
+
+      useEffect(() => {
+  if (backInfo) {
+    newBackground();
+  }
+}, [backInfo]);
     
     return (
 <div>
@@ -27,7 +34,7 @@ export default function AllTeams(){
      key={team.name}
      to={`/teams/${team.name}`} 
      id={team.name}   
-     onMouseEnter={()=>{setBackInfo(team.logo);newBackground();}} 
+     onMouseEnter={()=>{newBackground(team.logo);}} 
      style={{cursor:"pointer",backgroundColor:team.primaryColor,color:team.secondaryColor}}>
       {team.name}
       <img src={team.logo} alt={team.name} className="teamlogo"/>
--- src/TeamDetails.jsx
+++ src/TeamDetails.jsx
@@ -31,9 +31,37 @@ return(
     <h2 style={{backgroundColor:"white",textAlign:"center"}}>{displayteam.name}</h2>
     {/*<img className="displaylogo2"src={displayteam.logo} />*/}
     <div>Players (Or Position Groups)</div>
-    <div>Coaches</div>
-{team.HeadCoach}
+
+
+    <div className="databaseinfo" style={{display:"inline-block"}}>
+        <h3>Coaches</h3>
+<div className="valueborders" style={{borderColor:displayteam.secondaryColor}}>{team.HeadCoach}</div>
+</div>
+
+
+<div className="databaseinfo" style={{display:"inline-block"}}>
+        <h3>Offensive Cordinator</h3>
+<div className="valueborders" style={{borderColor:displayteam.secondaryColor}}>{team.OffCord}</div>
+</div>
+
+<div className="databaseinfo" style={{display:"inline-block"}}>
+        <h3>Defensive Cordinator</h3>
+<div className="valueborders" style={{borderColor:displayteam.secondaryColor}}>{team.DefCord}</div>
+</div>
+
+
     <div>Play Style</div>
+
+
+<div className="databaseinfo" style={{display:"inline-block"}}>
+        <h3>Offensive Scheme</h3>
+<div className="valueborders" style={{borderColor:displayteam.secondaryColor}}>{team.OffScheme}</div>
+</div>
+
+<div className="databaseinfo" style={{display:"inline-block"}}>
+        <h3>Defensive Scheme</h3>
+<div className="valueborders" style={{borderColor:displayteam.secondaryColor}}>{team.DefScheme}</div>
+</div>
     <div>Key Concepts</div>
     <div>Notes on Recent Game</div>
     <div>Key Comments</div>
